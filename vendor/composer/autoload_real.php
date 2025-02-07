7675be41189829316be1e13ec577a0abdb8beed7
gmitsos <gmitsos@epignosishq.com>
2025-02-07 10:40:38 +0200

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit87e2b26e1a40c74e49432d6b1829526a
             return self::$loader;
         }
 
+        require __DIR__.'/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit87e2b26e1a40c74e49432d6b1829526a', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit87e2b26e1a40c74e49432d6b1829526a', 'loadClassLoader'));
